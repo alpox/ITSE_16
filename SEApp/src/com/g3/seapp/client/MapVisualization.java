0c4c022f44a81414e599771436f9e31f6a62dbf8
AranMor <thomas.huber2@uzh.ch>
2016-11-14 18:09:06 +0100

    Added Worldmap to website

--- SEApp/src/com/g3/seapp/client/MapVisualization.java
+++ SEApp/src/com/g3/seapp/client/MapVisualization.java
@@ -4,8 +4,22 @@ import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Widget;
 
-public class MapVisualization implements IVisualization, IExportable {
 
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.googlecode.gwt.charts.client.ChartLoader;
+import com.googlecode.gwt.charts.client.ChartPackage;
+import com.googlecode.gwt.charts.client.ColumnType;
+import com.googlecode.gwt.charts.client.DataTable;
+import com.googlecode.gwt.charts.client.geochart.GeoChart;
+import com.googlecode.gwt.charts.client.geochart.GeoChartColorAxis;
+import com.googlecode.gwt.charts.client.geochart.GeoChartOptions;
+
+
+public class MapVisualization implements IVisualization, IExportable {
+	
+	private GeoChart geoChart;
+	
 	@Override
 	public void export() {
 		// TODO Auto-generated method stub
@@ -19,14 +33,56 @@ public class MapVisualization implements IVisualization, IExportable {
 	}
 
 	@Override
-	public void drawVisualization(Panel container) {
+	public void drawVisualization(final Panel container) {
 		// TODO Auto-generated method stub
+		
+		ChartLoader chartLoader = new ChartLoader(ChartPackage.GEOCHART);
+		chartLoader.loadApi(new Runnable() {
+
+			@Override
+			public void run() {
+				// Create and attach the chart
+				geoChart = new GeoChart();
+				container.add(geoChart);
+				updateVisualization(container);
+			}
+		});
+		
+		container.setHeight("70vh");
+		container.setWidth("70vw");
 		container.add(new HTML("<span>This is the world map</span>"));
 	}
 
 	@Override
 	public void updateVisualization(Panel container) {
 		// TODO Auto-generated method stub
+		// Prepare the data
+		DataTable dataTable = DataTable.create();
+		dataTable.addColumn(ColumnType.STRING, "Country");
+		dataTable.addColumn(ColumnType.NUMBER, "Popularity");
+		dataTable.addRows(6);
+		dataTable.setValue(0, 0, "Germany");
+		dataTable.setValue(0, 1, 200);
+		dataTable.setValue(1, 0, "United States");
+		dataTable.setValue(1, 1, 300);
+		dataTable.setValue(2, 0, "Brazil");
+		dataTable.setValue(2, 1, 400);
+		dataTable.setValue(3, 0, "Canada");
+		dataTable.setValue(3, 1, 500);
+		dataTable.setValue(4, 0, "France");
+		dataTable.setValue(4, 1, 600);
+		dataTable.setValue(5, 0, "RU");
+		dataTable.setValue(5, 1, 700);
+
+		// Set options
+		GeoChartOptions options = GeoChartOptions.create();
+		GeoChartColorAxis geoChartColorAxis = GeoChartColorAxis.create();
+		geoChartColorAxis.setColors("green", "yellow", "red");
+		options.setColorAxis(geoChartColorAxis);
+		options.setDatalessRegionColor("gray");
+
+		// Draw the chart
+		geoChart.draw(dataTable, options);
 		
 	}
 
